2e48288391664a827093e105de1d1ea888cc9e5d
Charlie Robson <charlie.robson@b2m-solutions.com>
2019-05-22 14:36:55 +0100

    luring disco dollies

--- discodolly/discodolly.ino
+++ discodolly/discodolly.ino
@@ -6,17 +6,38 @@
 #define BUTTON_PIN 0
 
 CRGBArray<NUM_LEDS> leds;
+byte huey[NUM_LEDS];
 
-byte buttonState, mode, hue;
+byte buttonState, mode, discoBrite;
+
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, WS2812_PIN>(leds, NUM_LEDS);
   pinMode(WHITE_LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  buttonState = 0;
+
   mode = 0;
+  buttonState = 0;
+
+  changeLightColours(0);
+
+  for (int i = 0; i < 256; ++i) {
+    analogWrite(WHITE_LED_PIN, i);
+    delay(4);
+  }
 }
 
+
+void changeLightColours(byte sv) {
+  for (int i = 0; i < NUM_LEDS; ++i) {
+    huey[i] = random();
+    leds[i] = CHSV(huey[i], sv, sv);
+  }
+}
+
+
+void goSleepNow() {
+}
 
 
 byte updateButton(bool isPressed) {
@@ -25,42 +46,55 @@ byte updateButton(bool isPressed) {
   return buttonState & 3;
 }
 
+
 void changeMode() {
   mode = 1 - mode;
 
-  if (mode == 0) {
-    // no disco, go white
-    for (int i = 0; i < 1024; i += 2) {
-      leds.fadeToBlackBy(10);
+  for (int i = 0; i < 256; ++i) {
+    byte ii = 255 - i;
+    if (mode == 0) {
+      // no disco, go white
       analogWrite(WHITE_LED_PIN, i);
-      FastLED.delay(1);
-    }
-  }
-  else {
-    // no white, go disco
-    for (int i = 0; i < 1024; i += 2) {
-      analogWrite(WHITE_LED_PIN, 1022 - i);
       for (int j = 0; j < NUM_LEDS; j++) {
-        leds[i] = CHSV(hue++, i / 4, i / 4);
+        leds[j] = CHSV(huey[j], ii, ii);
       }
-      FastLED.delay(1);
     }
+    else {
+      // no white, go disco
+      analogWrite(WHITE_LED_PIN, ii);
+      for (int j = 0; j < NUM_LEDS; j++) {
+        leds[j] = CHSV(huey[j], i, i);
+      }
+    }
+    delay(4);
   }
 }
 
 
 void loop() {
-  if (updateButton(digitalRead(BUTTON_PIN) == LOW ? 1 : 0) == 2) {
+  static int dlnum = 0;
+  static int cycles = 0;
+  static int buttonTime = 0;
+
+  byte buttonCode = updateButton(digitalRead(BUTTON_PIN) == LOW ? 1 : 0);
+  if (buttonCode == 2) {
     // button was just pressed
     changeMode();
-  }
-
-  if ((millis() & 511) == 511) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CHSV(hue++, 255, 255);
+  } else if (buttonCode == 3) {
+    // button held
+    buttonTime += 10;
+    if (buttonTime > 2000) {
+      goSleepNow();
     }
+  } else {
+    buttonTime = 0;
   }
 
-  FastLED.delay(33);
-}
+  if (++cycles == 16) {
+    changeLightColours(mode ? 255 : 0);
+    cycles = 0;
+  }
 
+  //FastLED.show();
+  delay(32);
+}
